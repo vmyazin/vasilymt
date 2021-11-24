6a13e7567f4cf046b53ab35e868cadaab8ead5a1
Vasily Myazin <vm@konspiredesign.com>
2021-11-24 17:46:28 -0300

    Test For Vasily

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+// Test
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
