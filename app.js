cc056817bc9a696a42b43005fe233ed9325d07d5
Vasily Myazin <productizement@gmail.com>
2024-11-14 11:52:53 -0300

    Update packages to modernize setup; replace Sass module

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
-let sassMiddleware = require('node-sass-middleware');
+const sassMiddleware = require("sass-middleware");
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
